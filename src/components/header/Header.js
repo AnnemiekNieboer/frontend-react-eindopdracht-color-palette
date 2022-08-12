9e318fad54f240bd2b49d94bef60406f2e35a4b7
AnnemiekNieboer <annemieknieboer@gmail.com>
2022-08-12 10:57:01 +0200

    finetuned header for desktop view

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Header.css"
 import Navigation from "../navigation/Navigation";
 
-function Header({className, header, text, children}) {
+function Header({header, text, children}) {
     return (
         <div className="outer-container__reusable header header-background-image__homepage">
-            <div className={className}>
+            <div className= "inner-container__reusable">
                 <Navigation/>
                 <section className="header__text-section">
                     <h1 className="header-text-section__title">{header}</h1>
